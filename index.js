d51b20cadf6c9b864b36552723d25d179fbf8270
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-21 11:33:37 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+// Scooter with year, color, and model properties
+// Driver with name, age, and experience properties
+// PickupLocation with address and city properties
+
+function Scooter(year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+  }
+  function Driver(name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+  }
+  function PickupLocation(address, city) {
+    this.address = address;
+    this.city = city;
+  }
+
+// class Scooter{
+//     constructor(year, color, model){
+//         this.year = year;
+//         this.color = color;
+//         this.model = model;
+//     }
+// }
+
+// class Driver{
+//     constructor(name, age, experience){
+//         this.name = name;
+//         this.age = age;
+//         this.experience = experience;
+//     }
+// }
+
+// class PickupLocation{
+//     constructor(address, city){
+//         this.address = address;
+//         this.city = city;
+//     }
+// }
